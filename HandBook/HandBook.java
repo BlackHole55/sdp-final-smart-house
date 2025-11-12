a92615c99c9fe174908e20b91e90d7a5c1c486bb
IsFariza <fariza.ismag@gmail.com>
2025-11-12 10:39:43 +0500

    feat: add handbook

---
+++ HandBook/HandBook.java
@@ -0,0 +1,20 @@
+package HandBook;
+
+public class HandBook {
+    private static final int DEFAULT_DAY_TEMPERATURE = 22;
+    private static final int DEFAULT_NIGHT_TEMPERATURE = 25;
+    private int temperature;
+
+    public int getTemperature() {
+        return temperature;
+    }
+    public void setDayTemperature() {
+        temperature = DEFAULT_DAY_TEMPERATURE;
+    }
+    public void setNightTemperature() {
+        temperature = DEFAULT_NIGHT_TEMPERATURE;
+    }
+
+
+
+}
