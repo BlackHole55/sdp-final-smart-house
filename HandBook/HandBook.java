c8bc5af5f792a0190c32f7b94c6d58c81341efe4
riri-anvarbekova <241194@astanait.edu.kz>
2025-11-13 11:00:48 +0500

    feat:add new message for HandBook

--- HandBook/HandBook.java
+++ HandBook/HandBook.java
@@ -15,7 +15,7 @@ public class HandBook {
         temperature = DEFAULT_NIGHT_TEMPERATURE;
     }
 
-    private static String THERMOSTAT_STATUS_MESSAGE = "Old Thermostat temperature: ";
+    private static final String THERMOSTAT_STATUS_MESSAGE = "Old Thermostat temperature: ";
 
     public static String getThermostatStatusMessage() {
         return THERMOSTAT_STATUS_MESSAGE;
