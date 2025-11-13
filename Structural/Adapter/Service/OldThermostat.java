c43a6bfc42349f7c5059d5593b5838a7f4a67e47
riri-anvarbekova <241194@astanait.edu.kz>
2025-11-13 11:07:47 +0500

    feat:add Old Termostat class

---
+++ Structural/Adapter/Service/OldThermostat.java
@@ -0,0 +1,7 @@
+package Structural.Adapter.Service;
+
+public class OldThermostat {
+    private double tempCelsius;
+    public void setTempCelsius(double temp) { this.tempCelsius = temp; }
+    public double getTempCelsius() { return tempCelsius; }
+}
