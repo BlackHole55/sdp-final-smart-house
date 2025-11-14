6babab7a6ba32225f139a10f36d5b99019bb9ae9
Meirhan Syzdykov <syzdykovmr@gmail.com>
2025-11-14 18:25:06 +0500

    feat: add Messages handbook

---
+++ MessagesHandbook/Messages.java
@@ -0,0 +1,15 @@
+package MessagesHandbook;
+
+public class Messages {
+    public static final String HEATING_IS_ON = "Heating system is on";
+    public static final String HEATING_IS_OFF = "Heating system is off";
+    public static final String LIGHTING_IS_ON = "Lighting system is on";
+    public static final String LIGHTING_IS_OFF = "Lighting system is off";
+
+    public static final String ALARM_IS_ACTIVE = "Alarms are activated";
+    public static final String ALARAM_IS_DEACTIVE = "Alarams are deactivated";
+
+    public static final String DOORS_LOCKED = "Doors are locked";
+    public static final String DOORS_UNLOCKED = "Doors are unclocked";
+    
+}
