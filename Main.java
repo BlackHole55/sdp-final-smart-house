6f928163bfe5faea7a0248fc0fdfa5766c251dcc
IsFariza <fariza.ismag@gmail.com>
2025-11-15 22:29:26 +0500

    feat: updated Main

--- Main.java
+++ Main.java
@@ -2,10 +2,12 @@ import Creational.Builder.LightingBuilder;
 import Creational.Builder.ThermostatBuilder;
 import Creational.Factory.SecurityCameraFactory;
 import Creational.Factory.SmokeDetectorFactory;
+import ConcreteDevices.AutomaticDoors;
 import ConcreteDevices.Lighting;
+import ConcreteDevices.SecurityCamera;
+import ConcreteDevices.SmokeDetector;
 import ConcreteDevices.Thermostat;
 import Devices.IDetector;
-import FacadeImplementation.ConcreteFacades.SecuritySystem;
 import FacadeImplementation.HomeManagerFacade;
 import StrategyImplementation.HomeModes.LeavingHomeMode;
 import HandBook.HandBook;
@@ -17,26 +19,35 @@ public class Main {
     public static void main(String[] args) {
         HandBook handBook = new HandBook();
 
-        HomeManagerFacade homeManager = new HomeManagerFacade(handBook);
-
         System.out.println("=== Device Builder===");
-        Lighting light = new LightingBuilder()
-                .withPowerState(true)
-                .withBrightness(75)
-                .build();
 
-        Thermostat thermostat = new ThermostatBuilder()
+        Lighting light1 = new LightingBuilder()
                 .withPowerState(true)
-                .withTemperature(23)
                 .build();
+        light1.setHandbook(handBook);
+        light1.setBrightness(23);
 
-        System.out.println("Lighting status: " + light.showStatus());
-        System.out.println("Lighting brightness: " + light.getBrightness());
-        System.out.println("Thermostat status: " + thermostat.showStatus());
-        System.out.println("Thermostat temperature: " + thermostat.getTemperature());
+        Thermostat thermostat1 = new ThermostatBuilder()
+                .withPowerState(true)
+                .build();
+        thermostat1.setHandbook(handBook);
+        thermostat1.setTemperature(50);
+
+        System.out.println(light1.showStatus());
+        System.out.println(thermostat1.showStatus());
         System.out.println();
 
+        AutomaticDoors doors1 = new AutomaticDoors();
+        SmokeDetectorFactory smokeDetectorFactory = new SmokeDetectorFactory();
+        IDetector smokeDetector1 = smokeDetectorFactory.createDetector();
+        SmokeDetector smokeDetectorCon1 = (SmokeDetector) smokeDetector1;
+
+        SecurityCameraFactory securityCameraFactory = new SecurityCameraFactory();
+        IDetector camera1 = securityCameraFactory.createDetector();
+        SecurityCamera cameraCon1 = (SecurityCamera) camera1;
+
         System.out.println("=== Home Mode Strategy ===");
+        HomeManagerFacade homeManager = new HomeManagerFacade(doors1, light1, cameraCon1, smokeDetectorCon1, thermostat1);
 
         System.out.println("\n-- Switching to Day Mode --");
         DayMode dayMode1 = new DayMode();
@@ -48,6 +59,23 @@ public class Main {
         homeManager.activateMode(nightMode1);
         homeManager.showStatus();
 
+        System.out.println();
+        System.out.println("=== Testing Detectors (Observer Pattern) ===");
+        
+        smokeDetector1.addObserver(new FireObserver(smokeDetectorCon1));
+        smokeDetector1.turnOn();
+        smokeDetector1.setDetected(true);
+        smokeDetector1.detect();
+
+        System.out.println();
+
+        camera1.addObserver(new PresenceDetectionObserver(cameraCon1));
+        camera1.turnOn();
+        camera1.setDetected(true);
+        camera1.detect();
+
+        System.out.println();
+
         System.out.println("\n-- Switching to Leaving Home Mode --");
         LeavingHomeMode leavingHomeMode1 = new LeavingHomeMode();
         homeManager.activateMode(leavingHomeMode1);
@@ -55,26 +83,7 @@ public class Main {
 
         System.out.println();
 
-        System.out.println("=== Observer Pattern ===");
-        SecuritySystem security = new SecuritySystem();
         
-        SmokeDetectorFactory smokeDetectorFactory = new SmokeDetectorFactory();
-        IDetector smoke = smokeDetectorFactory.createDetector();
-        smoke.addObserver(new FireObserver(security));
-        smoke.turnOn();
-        smoke.setDetected(true);
-        smoke.detect();
-
-        System.out.println();
-
-        SecurityCameraFactory securityCameraFactory = new SecurityCameraFactory();
-        IDetector camera = securityCameraFactory.createDetector();
-        camera.addObserver(new PresenceDetectionObserver(security));
-        camera.turnOn();
-        camera.setDetected(true);
-        camera.detect();
-
-        System.out.println();
 
         System.out.println("=== Final System Status ===");
         homeManager.showStatus();
