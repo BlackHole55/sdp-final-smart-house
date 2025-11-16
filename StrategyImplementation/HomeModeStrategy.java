fe513e5ef6c75b4438ba5f2c22850a9b9f7db0eb
Meirhan Syzdykov <syzdykovmr@gmail.com>
2025-11-16 18:09:44 +0500

    refactor: move strategy to behavioral directory

--- StrategyImplementation/HomeModeStrategy.java
+++
@@ -1,9 +0,0 @@
-package StrategyImplementation;
-
-import Devices.IDetector;
-import Devices.IDevice;
-import java.util.ArrayList;
-
-public interface HomeModeStrategy {
-    void activate(ArrayList<IDevice> devices, ArrayList<IDetector> detectors);
-}
