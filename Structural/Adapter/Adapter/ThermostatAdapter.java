29de040bbcb6a8045940ad729c94dfbb257639b8
riri-anvarbekova <241194@astanait.edu.kz>
2025-11-16 15:42:46 +0500

    feat:change extends BaseDevice to implemets IDevice

--- Structural/Adapter/Adapter/ThermostatAdapter.java
+++ Structural/Adapter/Adapter/ThermostatAdapter.java
@@ -1,10 +1,11 @@
 package Structural.Adapter.Adapter;
 
-import Devices.BaseDevice;
+import Devices.IDevice;
 import Structural.Adapter.Service.OldThermostat;
 import HandBook.HandBook;
+import MessagesHandbook.Messages;
 
-public class ThermostatAdapter extends BaseDevice {
+public class ThermostatAdapter implements IDevice {
     private OldThermostat oldThermostat;
 
     public ThermostatAdapter(OldThermostat oldThermostat) {
@@ -13,17 +14,23 @@ public class ThermostatAdapter extends BaseDevice {
 
     @Override
     public void turnOn() {
-        oldThermostat.setTempCelsius(HandBook.day_temperature);
+        oldThermostat.setTempCelsius(HandBook.DEFAULT_DAY_TEMPERATURE_CELSIUS);
     }
 
     @Override
     public void turnOff() {
-        oldThermostat.setTempCelsius(HandBook.night_temperature);
+        oldThermostat.setTempCelsius(HandBook.DEFAULT_NIGHT_TEMPERATURE_CELSIUS );
     }
 
     @Override
     public String showStatus() {
-        return HandBook.thermostat_status_message + oldThermostat.getTempCelsius() + " C";
+        return Messages.thermostat_status_message
+                + oldThermostat.getTempCelsius()
+                + HandBook.CELSIUS_STRING;
+    }
+    @Override
+    public boolean isOn() {
+        return oldThermostat.getTempCelsius() > 0;
     }
 }
 
